e45d266e05054bcff43e9de5e7f56d130f8452ea
timur-d-a <130913754+timur-d-a@users.noreply.github.com>
2023-11-23 21:13:36 +0500

    Add files via upload

---
+++ Рекурсия/Recursion.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+
+double x;
+
+double f(int n, int end, double fac) {
+    fac *= n;
+    double s = 1 + cos(n * M_PI / 4) / fac * x;
+    return n < end ? s + f(n + 1, end, fac) : s;
+}
+
+int main() {
+    double n;
+    cin >> x >> n;
+    cout << f(1, n, 1);
+    return 0;
+}
